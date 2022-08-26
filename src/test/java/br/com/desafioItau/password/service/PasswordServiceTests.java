bf6761c7156a9ff489e3eb8720ee640131200804
Rebeca Karinne Cavalcanti Lira <rebeca.lira@capgemini.com>
2022-08-26 16:51:15 -0300

    Finalização

--- src/test/java/br/com/desafioItau/password/service/PasswordServiceTests.java
+++ src/test/java/br/com/desafioItau/password/service/PasswordServiceTests.java
@@ -1,6 +1,10 @@
 package br.com.desafioItau.password.service;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -10,7 +14,6 @@ public class PasswordServiceTests {
     private PasswordService pS = new PasswordService();
 
     // Exemplos seguidos do challenge Itaú
-
     @Test
     void validationFalse() { // Exemplo de passwords não válidos
         assertFalse(pS.isValid(""));
@@ -22,8 +25,11 @@ public class PasswordServiceTests {
         assertFalse(pS.isValid("AbTp9 fok"));
     }
 
-    @Test
+    @Test // Exemplo de password válido
     void validationTrue() { // Exemplo de password válido
         assertTrue(pS.isValid("AbTp9!fok"));
     }
+
 }
+
+
